d329e3fb0a172ccde37c759907fb101e3c72aee7
valT-800 <valentina.oleskeviciute@stud.viko.lt>
2021-05-06 18:40:01 +0300

    Add project files.

---
+++ Uzd_N1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Uzd_N3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.fillButton = new System.Windows.Forms.Button();
+            this.attributesButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Catalog:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(35, 259);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(486, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resumeButton.Location = new System.Drawing.Point(356, 186);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(94, 23);
+            this.resumeButton.TabIndex = 19;
+            this.resumeButton.Text = "Resume";
+            this.resumeButton.UseVisualStyleBackColor = true;
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(224, 186);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(94, 23);
+            this.pauseButton.TabIndex = 18;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // fillButton
+            // 
+            this.fillButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fillButton.Location = new System.Drawing.Point(395, 118);
+            this.fillButton.Name = "fillButton";
+            this.fillButton.Size = new System.Drawing.Size(92, 23);
+            this.fillButton.TabIndex = 17;
+            this.fillButton.Text = "Fill";
+            this.fillButton.UseVisualStyleBackColor = true;
+            this.fillButton.Click += new System.EventHandler(this.fillButton_Click);
+            // 
+            // attributesButton
+            // 
+            this.attributesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attributesButton.Location = new System.Drawing.Point(395, 71);
+            this.attributesButton.Name = "attributesButton";
+            this.attributesButton.Size = new System.Drawing.Size(92, 25);
+            this.attributesButton.TabIndex = 20;
+            this.attributesButton.Text = "Add attribute";
+            this.attributesButton.UseVisualStyleBackColor = true;
+            this.attributesButton.Click += new System.EventHandler(this.attributesButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(93, 186);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(94, 23);
+            this.stopButton.TabIndex = 21;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Attributes:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 76);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 20);
+            this.textBox2.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "New ARFF file name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(137, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(233, 20);
+            this.textBox3.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 383);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.attributesButton);
+            this.Controls.Add(this.resumeButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.fillButton);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button resumeButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button fillButton;
+        private System.Windows.Forms.Button attributesButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Uzd_N1/Form1.cs
@@ -0,0 +1,187 @@
+﻿using ArffTools;
+using java.io;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Uzd_N3
+{
+    public partial class Form1 : Form
+    {
+
+        private int progress = 0;
+
+        private ManualResetEvent resetEvent = new ManualResetEvent(true);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void attributesButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("attributes.txt", true);
+            sw.WriteLine(textBox2.Text);
+            sw.Close();
+            MessageBox.Show("Attribute is added");
+        }
+        private void fillButton_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(Method)); //sukuriame parametrizuotą giją
+            thread.IsBackground = true;
+            thread.Start(textBox1.Text); // per parametrus perduodame path
+
+            int folderLength2 = Directory.GetFiles(textBox1.Text).Length;
+
+            Thread thread3 = new Thread(new ParameterizedThreadStart(Method3)); // per parametrus perduodame kiek iš viso yra šifruojamų failų --> koks max progresas
+            thread3.IsBackground = true;
+            thread3.Start(folderLength2);
+        }
+        void Method(object obj)
+        {
+            string folderPath = Convert.ToString(obj);
+
+            string[] allfiles = Directory.GetFiles(folderPath);
+
+            try
+            {
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    WriteArffFile(allfiles); 
+                });
+                resetEvent.WaitOne();
+                Thread.Sleep(100);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }            
+            
+        }
+        private void WriteArffFile(string[] allfiles)
+        {
+            using (ArffWriter arffWriter = new ArffWriter(textBox3.Text + ".arff"))
+            {
+                bool relation = true;
+                object[] inst = null;
+                object[][] insts = new object[allfiles.Length][];
+                int n = 0;
+
+                foreach (string file in allfiles)
+                {
+                    FileInfo fi = new FileInfo(file);
+
+                    ArffReader datafile = new ArffReader(file);
+
+                    ArffHeader header = datafile.ReadHeader();
+
+                    var instances = datafile.ReadAllInstances();
+
+                    //var instance = datafile.ReadInstance();
+
+                    
+                    object[] instance = null;
+
+                    foreach (var ins in instances) instance = ins;
+
+                    if (header != null)
+                    {
+
+                        var attributes = header.Attributes;
+
+                        string line;
+
+                        StreamReader attrfile = new StreamReader("attributes.txt");
+
+                        int i = 0;
+                        int j = 0;
+                        while ((line = attrfile.ReadLine()) != null)
+                        {
+                            foreach (var attribute in attributes)
+                            {
+                                if (attribute.Name == line)
+                                {
+                                    if (relation == true)
+                                    {
+                                        arffWriter.WriteRelationName(header.RelationName);
+                                        relation = false;
+                                    }
+
+                                    arffWriter.WriteAttribute(new ArffAttribute(attribute.Name, attribute.Type));
+
+                                    j++;
+                                }
+                                i++;
+                            }
+                        }
+                        
+                        inst = new object[j];
+                        j = 0;
+                        i = 0;
+
+                        while ((line = attrfile.ReadLine()) != null)
+                        {
+                            
+                            foreach (var attribute in attributes)
+                            {
+                                if (attribute.Name == line)
+                                {
+                                    inst.SetValue(instance.GetValue(i), j);
+                                    j++;
+                                }
+                                i++;
+                            }
+                        }
+                        
+                        insts.SetValue(inst,n);
+                        
+                        attrfile.Close();
+                    }
+                    datafile.Dispose();
+                    n++;
+                    progress++;
+                }
+                foreach(var ins in insts) arffWriter.WriteInstance( new object[] { ins });                
+            }
+        }
+        void Method3(object obj)
+        {
+            //kiek iš viso yra katalogų + failų perduodame per parametrus, darbo pradžioje
+            // pagal tai koks yra progress, apskaičiuojame procentaliai progressBar'o reikšmę
+            // IšViso - 100
+            // progress - x
+            // progress * 100 / išViso
+
+            int folderLength = Convert.ToInt32(obj);
+            while (progress <= folderLength)
+            {
+                this.BeginInvoke((MethodInvoker)delegate
+                {
+                    progressBar1.Value = progress * 100 / folderLength;
+                });
+                Thread.Sleep(100);
+            }
+            this.BeginInvoke((MethodInvoker)delegate { progressBar1.Value = 100; });
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            resetEvent.Reset();
+        }
+
+        private void resumeButton_Click(object sender, EventArgs e)
+        {
+            resetEvent.Set();
+        }
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            resetEvent.Close();
+        }
+    }
+}
